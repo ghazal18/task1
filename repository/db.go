e91080ee6381f975a9319ee5a711e4c82cd5ec8c
Ghazal Alikhani <ghazal.alikhani123@gmail.com>
2025-05-01 11:06:24 +0330

    clearer dead codes

--- repository/db.go
+++ repository/db.go
@@ -22,28 +22,6 @@ func New() *PostgresDB {
 }
 
 func (d *PostgresDB) Register(u entity.User) error {
-	/*
-
-		user1 := &entity.User{
-		//	ID:       9,
-			Email:    "sdfwewer@gmail",
-			Password: "345123",
-		}
-		_, err := d.db.Model(user1).Insert()
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = d.db.Model(&entity.User{
-		//	ID:       7,
-			Email:    "wefewew@gmail",
-			Password: "12334",
-		}).Insert()
-		if err != nil {
-			panic(err)
-		}
-	*/
-
 	_, err := d.db.Model(&u).Insert()
 	fmt.Println(err)
 	return nil
@@ -56,12 +34,5 @@ func (d *PostgresDB) GetUser(u entity.User) {
 	userEmail := u.Email
 	userPass := u.Password
 	res, err := d.db.Query(&u, userQuery, userEmail, userPass)
-	fmt.Println(res.RowsReturned(),res.Model(), u,err)
-
-	// err := d.db.Model(&u).from
-
-	// 	Where("users.email = ?", u.Email).
-	// 	Select()
-
-	// fmt.Println(u, err)
+	fmt.Println(res.RowsReturned(), res.Model(), u, err)
 }
